e625128ae0a5ba78af204efcef8daf6507cc88a1
Aleksandr <simd11092005@gmail.com>
2025-08-20 18:31:43 +0300

    Add files via upload

---
+++ Test_FastAPI_CRUD/src/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .tasks import (Task,
+                    TaskBase,
+                    TaskCreate,
+                    TaskUpdate)
+
+__all__ = ["Task",
+           "TaskBase",
+           "TaskCreate",
+           "TaskUpdate"]
